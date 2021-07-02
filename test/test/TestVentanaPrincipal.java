997f03ed287d3bf1c64ba31c9eaf1baa882daf65
Jorge Marin <jm.dev97@gmail.com>
2021-07-02 14:21:28 -0500

    Diseña pagina principal

---
+++ test/test/TestVentanaPrincipal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import vista.VentanaPrincipal;
+
+/**
+ *
+ * @author krypt97
+ */
+public class TestVentanaPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal vp = new VentanaPrincipal();
+        vp.setVisible(true);
+    }
+    
+}
