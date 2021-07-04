3fadf3fd78f2599b207747cc82134d1873a9c6ac
Jorge Marin <jm.dev97@gmail.com>
2021-07-03 19:15:08 -0500

    Modifica clase Usuario
    delete: atributo de clase privilegios.
    delete: constructor con parametros.
    add: atributo rol tipo string.
    add: constructor sin parametros.

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -10,108 +10,64 @@ package modelo;
  * @author willi
  */
 public class Usuario {
-    
-    private int dniUsuario;
+    // Variables de clase
+    private String dniUsuario;
     private String apellido;
     private String nombre;
     private String username;
     private String password;
-    private boolean privilegios;
+    private String rol;
     
-    public Usuario(int dniUsuario, String apellido,String nombre, String username, String password, boolean privilegios){
-        this.dniUsuario = dniUsuario;
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.username = username;
-        this.password = password;
-        this.privilegios = privilegios;
-    }
-
+    // Constructor
     public Usuario() {
     }
 
-    /**
-     * @return the dniUsuario
-     */
-    public int getDniUsuario() {
+    // Getters and Setters
+    public String getDniUsuario() {
         return dniUsuario;
     }
 
-    /**
-     * @param dniUsuario the dniUsuario to set
-     */
-    public void setDniUsuario(int dniUsuario) {
+    public void setDniUsuario(String dniUsuario) {
         this.dniUsuario = dniUsuario;
     }
 
-    /**
-     * @return the apellido
-     */
     public String getApellido() {
         return apellido;
     }
 
-    /**
-     * @param apellido the apellido to set
-     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
-    /**
-     * @return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the username
-     */
     public String getUsername() {
         return username;
     }
 
-    /**
-     * @param username the username to set
-     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-    /**
-     * @return the password
-     */
     public String getPassword() {
         return password;
     }
 
-    /**
-     * @param password the password to set
-     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    /**
-     * @return the privilegios
-     */
-    public boolean isPrivilegios() {
-        return privilegios;
+    public String getRol() {
+        return rol;
     }
 
-    /**
-     * @param privilegios the privilegios to set
-     */
-    public void setPrivilegios(boolean privilegios) {
-        this.privilegios = privilegios;
+    public void setRol(String rol) {
+        this.rol = rol;
     }
-   
 }
