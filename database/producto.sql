aa44b731abab12778d0bce3fa5b739ee0d07f95d
AlexTprog <alexander.t1780@gmail.com>
2021-07-05 14:04:57 -0500

    Tabla Producto y Categoria separadas

--- database/producto.sql
+++ database/producto.sql
@@ -16,30 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `categoria`
---
-
-DROP TABLE IF EXISTS `categoria`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `categoria` (
-  `idCategoria` int NOT NULL,
-  `nombreCategoria` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`idCategoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `categoria`
---
-
-LOCK TABLES `categoria` WRITE;
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'MONTAÑERAS'),(2,'RUTA'),(3,'URBANAS'),(4,'PLEGABLES'),(5,'BMX'),(6,'EQUIPO DE PROTECCION');
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `producto`
 --
 
@@ -77,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-05 13:41:28
+-- Dump completed on 2021-07-05 14:03:56
