98a51b9aec37777abd23e0a035e14416a12afbde
WilliamsEduardo12 <williams.cachique@unmsm.edu.pe>
2021-07-23 19:50:38 -0500

    Implementa Pedido y PedidoDAO
    [add] crea las clases y metodos sql necesarios para ambas clases.

--- test/test/TestClienteDAO.java
+++ test/test/TestClienteDAO.java
@@ -6,8 +6,9 @@
 package test;
 
 import java.util.ArrayList;
-import modelo.dao.ClienteDAO;
-import modelo.vo.Cliente;
+import modelo.dao.CategoriaDAO;
+import modelo.dao.PedidoDAO;
+import modelo.vo.Pedido;
 
 /**
  *
@@ -20,25 +21,47 @@ public class TestClienteDAO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Cliente miCliente = new Cliente();
-        ClienteDAO miClienteDAO = new ClienteDAO();
+        Pedido miCliente = new Pedido();
+        PedidoDAO miClienteDAO = new PedidoDAO();
         
-        miCliente.setDniCliente("73367034");
-        miCliente.setApellido("MARIN EVANGELISTA");
-        miCliente.setNombre("JORGE LUIS");
-        miCliente.setRuc("18200275");
+        Pedido miCliente2 = new Pedido();
         
-        //miClienteDAO.registrarCliente(miCliente);
-        //miClienteDAO.eliminarCliente("73367034");
-        //System.out.println(miClienteDAO.buscarCliente("73367034").getApellido());
-        //miClienteDAO.modificarCliente(miCliente);
+        miCliente2.setIdPedido(1);
+        miCliente2.setIdVenta("10");
+        miCliente2.setIdProducto("120");
+        miCliente2.setCantidad(145);
         
-        ArrayList<Cliente> miArrayList;
-        miArrayList = miClienteDAO.listarClientes();
+        miCliente.setIdPedido(12);
+        miCliente.setIdVenta("10");
+        miCliente.setIdProducto("121");
+        miCliente.setCantidad(141);
+        
+        
+        
+        //miClienteDAO.registrarPedido(miCliente);
+        //miClienteDAO.registrarPedido(miCliente2);
+        //miClienteDAO.eliminarPedido(1000);
+        //System.out.println(miClienteDAO.buscarPedido("1000").getCantidad());
+        //miClienteDAO.modificarPedido(miCliente);
+        
+        /*
+        ArrayList<Pedido> miArrayList;
+        miArrayList = miClienteDAO.listarPedido();
         
         for (int i = 0; i < miArrayList.size(); i++) {
-            System.out.println(miArrayList.get(i).getApellido());
+            System.out.println(miArrayList.get(i).getIdVenta());
+        }*/
+        
+        System.out.println("\n\nListar Busqueda \n\n");
+        
+        ArrayList<Pedido> miArrayList;
+        miArrayList = miClienteDAO.buscarPedido("10");
+        
+        for (int i = 0; i < miArrayList.size(); i++) {
+            System.out.println(miArrayList.get(i).getIdVenta());
         }
+        
+        
     }
     
 }
