d2452ae193dc71344e27e03101686ce482a2f380
Jorge Marin <jm.dev97@gmail.com>
2021-07-27 16:06:14 -0500

    Conecta panel ventas en ventana principal
    [add] instancia panel ventas y lo conecta a la vista principal.
    [add] crea metodo cargar panel ventas en controlador general.
    [modify] mueve el metodo seter del controlador general los setbound
    de cada panel

--- src/aplicacion/BikeShop.java
+++ src/aplicacion/BikeShop.java
@@ -9,13 +9,16 @@ import controlador.ControladorGeneral;
 import controlador.ControladorProducto;
 import controlador.ControladorProveedor;
 import controlador.ControladorUsuario;
+import controlador.ControladorVenta;
 import modelo.dao.CategoriaDAO;
 import modelo.dao.ProductoDAO;
 import modelo.dao.ProveedorDAO;
 import modelo.dao.UsuarioDAO;
+import modelo.dao.VentaDAO;
 import vista.PanelProducto;
 import vista.PanelProveedores;
 import vista.PanelUsuarios;
+import vista.PanelVentas;
 import vista.VentanaPrincipal;
 
 /**
@@ -29,59 +32,75 @@ public class BikeShop {
      */
     public static void main(String[] args) {
         // ÚNICA INSTANCIA DE LOS ELEMENTOS A CONECTAR
+        
+        // [vista]
         VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
         PanelUsuarios miPanelUsuarios = new PanelUsuarios();
         PanelProducto MiPanelProducto = new PanelProducto();
         PanelProveedores miPanelProveedores = new PanelProveedores();
+        PanelVentas miPanelVentas = new PanelVentas();
+        
+        // [dao]
         UsuarioDAO miUsuarioDAO = new UsuarioDAO();
         ProductoDAO miProductoDAO = new ProductoDAO();
         ProveedorDAO miProveedorDAO = new ProveedorDAO();
+        VentaDAO miVentaDAO = new VentaDAO();
+        
+        // [controlador]
         ControladorUsuario miControladorUsuario = new ControladorUsuario();
         ControladorProducto miControladorProducto = new ControladorProducto();
         ControladorProveedor miControladorProveedor = new ControladorProveedor();
+        ControladorVenta miControladorVenta = new ControladorVenta();
+        
         ControladorGeneral miControladorGeneral = new ControladorGeneral();
 
-        
-
+/* ---------------------------------------------------------------------------------------- */
         // CONECTANDO CONTROLADOR A VISTAS Y/O MODELOS
+        
+        // [vistas]
         miControladorUsuario.setPanelUsuarios(miPanelUsuarios);
         miControladorProveedor.setPanelProveedores(miPanelProveedores);
         miControladorProducto.setPanelProducto(MiPanelProducto);
+        
+        // [dao]
         miControladorUsuario.setUsuarioDAO(miUsuarioDAO);
         miControladorProveedor.setProveedorDAO(miProveedorDAO);
         miControladorProducto.SetProductoDAO(miProductoDAO);
+        
+        // [general vistas]
         miControladorGeneral.setPanelUsuarios(miPanelUsuarios);
         miControladorGeneral.setPanelProveedores(miPanelProveedores);
         miControladorGeneral.setPanelProducto(MiPanelProducto);
+        miControladorGeneral.setPanelVentas(miPanelVentas);
         miControladorGeneral.setVentanaPrincipal(miVentanaPrincipal);
 
-        
-
+/* ---------------------------------------------------------------------------------------- */
         // CONECTANDO VISTAS Y MODELOS AL CONTROLADOR
+        
+        // [vistas]
         miPanelUsuarios.setControlador(miControladorUsuario);
         MiPanelProducto.setControlador(miControladorProducto);
         miPanelProveedores.setControlador(miControladorProveedor);
+        
+        // [dao]
         miUsuarioDAO.setControlador(miControladorUsuario);
         miProductoDAO.setControladorProducto(miControladorProducto);
         miProveedorDAO.setControladorProveedor(miControladorProveedor);
         miVentanaPrincipal.setControladorGeneral(miControladorGeneral);
 
-        
-        
+/* ---------------------------------------------------------------------------------------- */
         // CARGANDO PANELES
         miVentanaPrincipal.add(miPanelUsuarios);
         miVentanaPrincipal.add(MiPanelProducto);
         miVentanaPrincipal.add(miPanelProveedores);
+        miVentanaPrincipal.add(miPanelVentas);
         
-
-        
+/* ---------------------------------------------------------------------------------------- */
         // SETEANDO TABLAS
         miPanelUsuarios.setTabla(miUsuarioDAO.listarUsuarios());
         miPanelProveedores.setTabla(miProveedorDAO.mostrarProveedores());
         MiPanelProducto.setTablaProductos(miControladorProducto.mostrarProductos());
         MiPanelProducto.setTablaCategoria(CategoriaDAO.mostrarCategorias());
         MiPanelProducto.colocarCategorias(CategoriaDAO.mostrarCategorias());
-        
-        miVentanaPrincipal.setVisible(true);
     }
 }
